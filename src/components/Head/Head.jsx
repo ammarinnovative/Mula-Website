6f51c18ca12397607332d95486441f8f367181c7
ammarinnovative <126662033+ammarinnovative@users.noreply.github.com>
2023-08-29 22:29:18 +0500

    updated code

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -18,7 +18,7 @@ import img from '../../assets/images/img.png';
 
 export default function CallToActionWithVideo() {
   return (
-    <Box bg={'#00000f'} height={"100vh"}>
+    <Box bg={'#00000f'} height={'100vh'}>
       <Container maxW={'7xl'}>
         <Stack
           align={'center'}
@@ -26,16 +26,21 @@ export default function CallToActionWithVideo() {
           py={{ base: 20, md: 28 }}
           direction={{ base: 'column', md: 'row' }}
         >
-          <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+          <Stack
+            textAlign={{ base: 'center', md: 'left' }}
+            flex={1}
+            spacing={{ base: 5, md: 10 }}
+          >
             <Heading
               lineHeight={1.1}
               fontWeight={600}
+              textAlign={{ base: 'center', md: 'left' }}
               fontSize={{ base: '3xl', sm: '4xl', lg: '4xl' }}
             >
               <Text
                 as={'span'}
                 position={'relative'}
-                color={"white"}
+                color={'white'}
                 _after={{
                   content: "''",
                   width: 'full',
@@ -44,7 +49,7 @@ export default function CallToActionWithVideo() {
                   bottom: 1,
                   left: 0,
                   bg: 'white',
-                  color:"white",
+                  color: 'white',
                   zIndex: -1,
                 }}
               >
@@ -55,7 +60,7 @@ export default function CallToActionWithVideo() {
                 Virtual Consultation
               </Text>
             </Heading>
-            <Text color={'gray.500'}>
+            <Text textAlign={{ base: 'center', md: 'left' }} color={'gray.500'}>
               Snippy is a rich coding snippets app that lets you create your own
               code snippets, categorize them, and even sync them in the cloud so
               you can use them anywhere. All that is free!
@@ -89,7 +94,6 @@ export default function CallToActionWithVideo() {
               width={'full'}
               overflow={'hidden'}
             >
-              
               <Image
                 alt={'Hero Image'}
                 fit={'cover'}
--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -84,110 +84,145 @@ const Contact = () => {
         >
           <Text
             textAlign={'center'}
-            mb={'25px'}
-            mt={'-15px'}
+            mb={{base:"0px",md:'25px'}}
+            mt={{base:"none",md:'-15px'}}
             fontWeight={'700'}
-            fontSize={'40px'}
+            fontSize={{base:"27px",md:"35px",lg:"40px"}}
             color={'white'}
           >
             Contact Us
           </Text>
         </Box>
-        <Box>
-          <Box display={'flex'} gap={"20px"} justifyContent={"center"} alignItems={"center"}>
-            <Box flex={'5'}>
-              <Text
-                m={'10px 0'}
-                fontSize={'35px'}
-                fontWeight={'700'}
+
+        <Box
+          display={'flex'}
+          flexWrap={'wrap'}
+          gap={'20px'}
+          flexDirection={{ base: 'column', lg: 'row' }}
+          justifyContent={{ base: 'left', lg: 'center' }}
+          alignItems={'center'}
+        >
+          <Box flex={'5'}>
+            <Text
+              m={'10px 0'}
+              textAlign={{ base: 'center', md: 'left' }}
+              fontSize={{ base: '25px', md: '35px' }}
+              fontWeight={'700'}
+              color={'white'}
+              mb={'30px'}
+            >
+              Get in touch with us <br />{' '}
+              <Text as={'span'} borderBottom={'3px solid #1f2bcc'}>
+                for any
+              </Text>{' '}
+              kind of Query
+            </Text>
+            <Text
+              mt={'20px 0'}
+              textAlign={{ base: 'center', md: 'left' }}
+              fontSize={{base:"14px",md:"17px"}}
+              color={'#a6a6a6'}
+            >
+              Sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt. Labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud ercitation.
+            </Text>
+            <Box
+              mt={'20px'}
+              display={'flex'}
+              flexDirection={'column'}
+              gap={'15px'}
+            >
+              <Input
+                onChange={e => {
+                  setField({ ...field, name: e.target.value });
+                }}
                 color={'white'}
-                mb={'30px'}
+                placeholder="Name"
+                outline={'none'}
+                padding={'25px 10px'}
+                placeholderTextColor="white"
+                borderColor={'#1f2bcc'}
+                type="text"
+              />
+              <Input
+                border={'1px solid #ddd'}
+                color={'white'}
+                placeholder="Email"
+                padding={'25px 10px'}
+                placeholderTextColor="white"
+                borderColor={'#1f2bcc'}
+                type="Email"
+                onChange={e => {
+                  setField({ ...field, email: e.target.value });
+                }}
+              />
+              <Textarea
+                color={'white'}
+                padding={'15px 10px'}
+                placeholderTextColor="white"
+                borderColor={'#1f2bcc'}
+                height={'20vh'}
+                onChange={e => {
+                  setField({ ...field, message: e.target.value });
+                }}
+                placeholder="Message"
+              ></Textarea>
+              <Button
+                onClick={sendMessage}
+                backgroundColor={'#041689'}
+                color={'white'}
+                _hover={'none'}
               >
-                Get in touch with us <br />{' '}
-                <Text as={'span'} borderBottom={'3px solid #1f2bcc'}>
-                  for any
-                </Text>{' '}
-                kind of Query
-              </Text>
-              <Text mt={'20px 0'} fontSize={'17px'} color={'#a6a6a6'}>
-                Sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt. Labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud ercitation.
-              </Text>
-              <Box
-                mt={'20px'}
-                display={'flex'}
-                flexDirection={'column'}
-                gap={'15px'}
-              >
-                <Input
-                  onChange={e => {
-                    setField({ ...field, name: e.target.value });
-                  }}
-                  color={'white'}
-                  placeholder="Name"
-                  outline={'none'}
-                  padding={'25px 10px'}
-                  placeholderTextColor="white"
-                  borderColor={'#1f2bcc'}
-                  type="text"
-                />
-                <Input
-                  border={'1px solid #ddd'}
-                  color={'white'}
-                  placeholder="Email"
-                  padding={'25px 10px'}
-                  placeholderTextColor="white"
-                  borderColor={'#1f2bcc'}
-                  type="Email"
-                  onChange={e => {
-                    setField({ ...field, email: e.target.value });
-                  }}
-                />
-                <Textarea
-                  color={'white'}
-                  padding={'15px 10px'}
-                  placeholderTextColor="white"
-                  borderColor={'#1f2bcc'}
-                  height={'20vh'}
-                  onChange={e => {
-                    setField({ ...field, message: e.target.value });
-                  }}
-                  placeholder="Message"
-                ></Textarea>
-                <Button
-                  onClick={sendMessage}
-                  backgroundColor={'#041689'}
-                  color={'white'}
-                  _hover={'none'}
-                >
-                  Send
-                </Button>
-              </Box>
-              <Box backgroundColor={'#00000f'} width={'100%'} mt={'40px'}></Box>
+                Send
+              </Button>
             </Box>
-            <Box  flex={'2'}>
-              <Text fontSize={"30px"} color={"white"} fontWeight={"800"}>Information</Text>
-              <Text color={"white"} fontWeight={"500"} fontSize={"24px"}>Contact Us</Text>
-              <Box display={"flex"}  mt={"20px"} flexDirection={"column"} gap={"30px"}>
-                <Box gap={"20px"} alignItems={"center"} display={"flex"}>
-                  <FaLocationArrow color='#12158a' />
-                  <Text color={"#474747"} fontSize={"18px"} >Address: 77 Indian St. perth, NY</Text>
-                </Box>
-                <Box gap={"20px"} alignItems={"center"} display={"flex"}>
-                  <AiTwotonePhone color='#12158a' />
-                  <Text color={"#474747"} fontSize={"18px"}>Phone: +3233-332-334</Text>
-                </Box>
-                <Box gap={"20px"} alignItems={"center"} display={"flex"}>
-                  <AiOutlineMail color='#12158a' />
-                  <Text color={"#474747"} fontSize={"18px"}>Email: mula@gmail.com</Text>
-                </Box>
-                <Box>
-                  <Text color={"white"} fontWeight={"700"} fontSize={"26px"}>Social Media</Text>
-                  <Box display={"flex"} mt={"10px"} gap={"20px"}>
-                    <Box><FaFacebook color='white' fontSize={"30px"} /></Box>
-                    <Box><AiOutlineInstagram color='white' fontSize={"30px"} /></Box>
-                    <Box><AiOutlineTwitter color='white' fontSize={"30px"} /></Box>
+            <Box backgroundColor={'#00000f'} width={'100%'} mt={'40px'}></Box>
+          </Box>
+          <Box flex={'2'} width={'100%'}>
+            <Text fontSize={'30px'} color={'white'} fontWeight={'800'}>
+              Information
+            </Text>
+            <Text color={'white'} fontWeight={'500'} fontSize={'24px'}>
+              Contact Us
+            </Text>
+            <Box
+              display={'flex'}
+              mt={'20px'}
+              flexDirection={'column'}
+              gap={'30px'}
+            >
+              <Box gap={'20px'} alignItems={'center'} display={'flex'}>
+                <FaLocationArrow color="#12158a" />
+                <Text color={'#474747'} fontSize={'18px'}>
+                  Address: 77 Indian St. perth, NY
+                </Text>
+              </Box>
+              <Box gap={'20px'} alignItems={'center'} display={'flex'}>
+                <AiTwotonePhone color="#12158a" />
+                <Text color={'#474747'} fontSize={'18px'}>
+                  Phone: +3233-332-334
+                </Text>
+              </Box>
+              <Box gap={'20px'} alignItems={'center'} display={'flex'}>
+                <AiOutlineMail color="#12158a" />
+                <Text color={'#474747'} fontSize={'18px'}>
+                  Email: mula@gmail.com
+                </Text>
+              </Box>
+              <Box>
+                <Text color={'white'} fontWeight={'700'} fontSize={'26px'}>
+                  Social Media
+                </Text>
+                <Box display={'flex'} mt={'10px'} gap={'20px'}>
+                  <Box>
+                    <FaFacebook color="white" fontSize={'30px'} />
+                  </Box>
+                  <Box>
+                    <AiOutlineInstagram color="white" fontSize={'30px'} />
+                  </Box>
+                  <Box>
+                    <AiOutlineTwitter color="white" fontSize={'30px'} />
                   </Box>
                 </Box>
               </Box>
