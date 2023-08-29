cee04ae98d681f55e94e13d5d43d0b7fd6667a05
ammarinnovative <126662033+ammarinnovative@users.noreply.github.com>
2023-08-29 16:03:26 +0500

    updated code

--- src/views/Consultation/Consultation.jsx
+++ src/views/Consultation/Consultation.jsx
@@ -9,105 +9,103 @@ import Tophead from '../../components/Tophead/Tophead';
 import { POST } from '../../utilities/ApiProvider';
 import { Toast } from '@chakra-ui/react';
 const Consultation = () => {
-
-  const [bool,setBool] = useState(false);
-  const [field,setField] = useState({
-    name:"",
-    email:"",
-    time:"",
-    date:"",
-    type:"consultation_inquiry",
-    message:""
+  const [bool, setBool] = useState(false);
+  const [field, setField] = useState({
+    name: '',
+    email: '',
+    time: '',
+    date: '',
+    type: 'consultation_inquiry',
+    message: '',
   });
   const toast = useToast();
-  const SendData = async ()=>{
+  const SendData = async () => {
     setBool(true);
-    if(!field.name || !field.email || !field.time || !field.date || !field.message){
+    if (
+      !field.name ||
+      !field.email ||
+      !field.time ||
+      !field.date ||
+      !field.message
+    ) {
       toast({
-        position:"bottom-left",
-        isClosable:true,
-        status:"error",
-        duration:5000,
-        description:"Please fill all the fields"
+        position: 'bottom-left',
+        isClosable: true,
+        status: 'error',
+        duration: 5000,
+        description: 'Please fill all the fields',
       });
       setBool(false);
     }
     try {
-      const res = await POST("web/inquiry",field);
-      if(res.status ==200){
+      const res = await POST('web/inquiry', field);
+      if (res.status == 200) {
         setField({
           ...field,
-          name:"",
-          email:"",
-          time:"",
-          date:"",
-          message:""
-        })
-        toast({
-          position:"bottom-left",
-        isClosable:true,
-        status:"success",
-        duration:5000,
-        description:"Message Send Successfulyy"
+          name: '',
+          email: '',
+          time: '',
+          date: '',
+          message: '',
         });
-        
-      }else{
         toast({
-          position:"bottom-left",
-        isClosable:true,
-        status:"error",
-        duration:5000,
-        description:res.status.message
-        })
+          position: 'bottom-left',
+          isClosable: true,
+          status: 'success',
+          duration: 5000,
+          description: 'Message Send Successfulyy',
+        });
+      } else {
+        toast({
+          position: 'bottom-left',
+          isClosable: true,
+          status: 'error',
+          duration: 5000,
+          description: res.status.message,
+        });
       }
     } catch (error) {
       toast({
-        position:"bottom-left",
-        isClosable:true,
-        status:"error",
-        duration:5000,
-        description:error
-      })
+        position: 'bottom-left',
+        isClosable: true,
+        status: 'error',
+        duration: 5000,
+        description: error,
+      });
     }
-   
-  }
+  };
 
   return (
-  
     <Box position={'relative'} width={'100%'}>
-       <Box
-      
-      backgroundColor={'#00000f'}
-   padding={'200px 0 70px '}
-      width={'100%'}
-      
-    >
-      <Text textAlign={"center"} mb={"25px"} mt={"-15px"} fontWeight={"700"} fontSize={"37px"} color={"white"}>Visual Consultation</Text>
+      <Box backgroundColor={'#00000f'} padding={{base:"150px 0 0px",md:"200px 0 10px",lg:'200px 0 5 px '}} width={'100%'}>
+        <Text
+          textAlign={'center'}
+          mb={{base:"0px",md:"15px",lg:"20px"}}
+          fontWeight={'700'}
+          fontSize={{base:"23px",md:"27px",lg:"33px"}}
+          color={'white'}
+        >
+          Visual Consultation
+        </Text>
       </Box>
       <Box>
         <CallToActionWithVideo />
         <Box
           backgroundImage={back}
           backgroundPosition={'center'}
-          height={'30vh'}
           width={'100%'}
-          padding={'10px'}
+          padding={'30px 10px'}
           backgroundSize={'cover'}
+          flexDirection={{ base: 'column', lg: 'row' }}
           display={'flex'}
           justifyContent={'space-evenly'}
           gap={'30px'}
         >
-          <Box width={'35%'}>
-            <Text fontSize={'27px'} color={'white'}>
+          <Box textAlign={{base:"center",lg:"none"}} width={{ base: '100%', lg: '35%' }}>
+            <Text mb={{base:"30px",lg:"none"}} textAlign={{base:"center",lg:"none"}} fontSize={'27px'} color={'white'}>
               Confusing get a Facetime <br />
-              <Text as={'span'}></Text>Meeting With Mark Mula
+              <Text as={'span'} borderBottom={"4px solid blue"}>Meeting</Text> With Mark Mula
             </Text>
-            <Box
-              width={'10%'}
-              m={'10px 0'}
-              height={'3px'}
-              backgroundColor={'blue'}
-            ></Box>
             <Text>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
               magni aliquid quasi libero hic atque, voluptas quibusdam
@@ -116,28 +114,65 @@ const Consultation = () => {
             </Text>
           </Box>
           <Box
-            width={'40%'}
-            height={'45vh'}
+            width={{ base: '100%', lg: '40%' }}
             padding={'10px'}
             boxShadow="0px 0px 10px 2px rgba(0, 0, 255, 0.3)" // Set the blue box shadow
             borderRadius="md"
-            mt={'-70px'}
+            mt={{ base: 'none', lg: '-70px' }}
             zIndex={'2'}
             backgroundColor={'#000000'}
           >
             <Input
               border={'1px solid blue'}
               type="text"
-              color={"white"}
-              onChange={(e)=>{setField({...field,name:e.target.value})}}
+              color={'white'}
+              onChange={e => {
+                setField({ ...field, name: e.target.value });
+              }}
               m={'5px 0'}
               value={field.name}
               placeholder="Name"
             />
-            <Input type="email" m={'5px 0'} value={field.email} color={"white"} onChange={(e)=>{setField({...field,email:e.target.value})}} placeholder="Email" />
-            <Input type="time" m={'5px 0'} value={field.time}  color={"white"} onChange={(e)=>{setField({...field,time:e.target.value})}} placeholder="Time" />
-            <Input type="date" m={'5px 0'} value={field.date} color={"white"} onChange={(e)=>{setField({...field,date:e.target.value})}} placeholder="Date" />
-            <Textarea width="100%" m={'5px 0'} color={"white"}value={field.message} onChange={(e)=>{setField({...field,message:e.target.value})}} placeholder="Message"></Textarea>
+            <Input
+              type="email"
+              m={'5px 0'}
+              value={field.email}
+              color={'white'}
+              onChange={e => {
+                setField({ ...field, email: e.target.value });
+              }}
+              placeholder="Email"
+            />
+            <Input
+              type="time"
+              m={'5px 0'}
+              value={field.time}
+              color={'white'}
+              onChange={e => {
+                setField({ ...field, time: e.target.value });
+              }}
+              placeholder="Time"
+            />
+            <Input
+              type="date"
+              m={'5px 0'}
+              value={field.date}
+              color={'white'}
+              onChange={e => {
+                setField({ ...field, date: e.target.value });
+              }}
+              placeholder="Date"
+            />
+            <Textarea
+              width="100%"
+              m={'5px 0'}
+              color={'white'}
+              value={field.message}
+              onChange={e => {
+                setField({ ...field, message: e.target.value });
+              }}
+              placeholder="Message"
+            ></Textarea>
             <Button
               width={'100%'}
               m={'5px 0'}
@@ -149,13 +184,11 @@ const Consultation = () => {
             </Button>
           </Box>
         </Box>
-        <Box backgroundColor={"#00000f"} paddingTop={"50px"}>
-        <Banner />
-        <Footer />
+        <Box backgroundColor={'#00000f'} paddingTop={'50px'}>
+          <Banner />
+          <Footer />
         </Box>
       </Box>
-      
-      
     </Box>
   );
 };
